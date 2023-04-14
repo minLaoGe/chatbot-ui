af139ea5705316ac93f8d705e89dc399f8f6ce54
闵逢雨 <5111234+minfengyu@user.noreply.gitee.com>
2023-04-14 16:41:16 +0800

    modify project pre

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const { i18n } = require('./next-i18next.config');
-
+function getBasePath() {
+  return '/gpt'
+}
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
   reactStrictMode: true,
-
+  basePath: getBasePath(),
   webpack(config, { isServer, dev }) {
     config.experiments = {
       asyncWebAssembly: true,
